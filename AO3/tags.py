fc5de6a6a33218591c48595780ece4548ac000d3
AJ <gr18578>
2024-08-30 03:10:06 +0100

    temp: reconfigured tags + added worktag

--- AO3/tags.py
+++ AO3/tags.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from bs4 import BeautifulSoup
+from typing import Optional
+import hashlib
 
 class TagType(Enum):
     RELATIONSHIPS = "relationships"
@@ -7,20 +9,36 @@ class TagType(Enum):
     FREEFORMS = "freeforms"
 
 class Tag:
-    def __init__(self, *args):
-        def constructor1(name: str, tag_type: TagType) -> None:
-            self.name = name
-            self.type = tag_type
+    # def __init__(self, *args):
+    #     def constructor1(name: str, tag_type: TagType) -> None:
+    #         self.name = name
+    #         self.type = tag_type
         
-        def constructor2(name: str, soup: BeautifulSoup) -> None:
-            self.name = name
-            list_items = soup.find_all("a", class_="tag", string=name) # TODO: what if none, account for that
-            tag_type = list_items[0].find_parent().get("class")[0]
-            self.type = TagType(tag_type)
+    #     def constructor2(name: str, soup: BeautifulSoup) -> None:
+    #         self.name = name
+    #         list_items = soup.find_all("a", class_="tag", string=name) # TODO: what if none, account for that
+    #         tag_type = list_items[0].find_parent().get("class")[0]
+    #         self.type = TagType(tag_type)
 
-        if (len(args) == 2) and (type(args[0]) is str) and (type(args[1]) is TagType):
-            constructor1(args[0], args[1])
-        elif (len(args) == 2) and (type(args[0]) is str) and (type(args[1]) is BeautifulSoup):
-            constructor2(args[0], args[1])
+    #     if (len(args) == 2) and (type(args[0]) is str) and (type(args[1]) is TagType):
+    #         constructor1(args[0], args[1])
+    #     elif (len(args) == 2) and (type(args[0]) is str) and (type(args[1]) is BeautifulSoup):
+    #         constructor2(args[0], args[1])
         
+    def __init__(self, name: str, tag_type: TagType) -> None:
+        self.name = name
+        self.type = tag_type
+    
+    def __eq__(self, other: "Tag"):
+        return (self.name == other.name) and (self.type == other.type)
 
+    def __hash__(self) -> int:
+        hex_hash = hashlib.md5(f"{self.name}_{self.type.value}".encode('utf-8')).hexdigest()
+        return int(hex_hash, 16)
+
+def find_tag_in_soup(tag_name: str, soup: BeautifulSoup) -> Optional[Tag]:
+    list_items = soup.find_all("a", class_="tag", string=tag_name) # TODO: what if none, account for that
+    if list_items is None:
+        return
+    tag_type = list_items[0].find_parent().get("class")[0] # TODO: check if found
+    return Tag(name=tag_name, tag_type=TagType(tag_type))
---
+++ AO3/work_tag.py
@@ -0,0 +1,48 @@
+"""Representation of a work through it's tags"""
+
+from .tags import TagType, Tag
+
+from bs4 import BeautifulSoup
+
+from typing import TypedDict
+
+class TagSection(TypedDict):
+    tag_type: TagType
+    tags: set
+
+class WorkTags:
+    def __init__(self, tags: set[Tag], sectioned_tags: TagSection) -> None:
+        self.tags = set(tags)
+        self.sectioned_tags = sectioned_tags
+
+def parse_worktags_from_html(soup: BeautifulSoup, debug: bool = False) -> list[WorkTags]:
+    results = soup.find("ol", {"class": ("work", "index", "group")})
+    if results is None:
+        return
+    works = []
+    for work in results.find_all("li", {"role": "article"}):
+        if work.h4 is None:
+            continue
+        tags_section = work.select_one("ul.tags.commas")
+        tags = tags_section.find_all("li", class_=[TagType.CHARACTERS.value, TagType.RELATIONSHIPS.value, TagType.FREEFORMS.value])
+        # always ordered Rel -> Char -> Freeform
+        sectioned_tags = {tag_type:set() for tag_type in TagType}
+        full_tag_set = set()
+        for tag in tags:
+            t = Tag(name=tag.text, tag_type=TagType(tag["class"][0]))
+            sectioned_tags[t.type].update([t])
+            full_tag_set.update([t])
+        
+        # create work representation
+        work = WorkTags(full_tag_set, sectioned_tags)
+        works.append(work)
+
+        # just for debug printing
+        if debug:
+            for sec in [work.tags] + list(work.sectioned_tags.values()):
+                print("______________section______________")
+                for tag in sec:
+                    print(f"{tag.type}: {tag.name}")
+                
+    print(len(works))
+    return works
